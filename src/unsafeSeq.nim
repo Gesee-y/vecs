e7d99efd643c329412e02072d7a5ebf309c0c1a8
Talom Lael <gesee37@gmail.com>
2026-09-01 06:26:50 +0100

    Remade VSeq but safer

--- src/unsafeSeq.nim
+++ src/unsafeSeq.nim
@@ -19,6 +19,17 @@
 # Internal layout accessors (not exported)
 # -----------------------------------------------------------------------------
 
+type
+  VSeqPayload[T] = object
+    cap: int
+    data: UncheckedArray[T]
+
+  VSeq*[T] = object
+    len: int
+    payload: ptr VSeqPayload[T]
+
+const CHECKS_ENABLED = not defined(danger)
+
 proc seqLenPtr(seqVar: pointer): ptr int {.inline.} =
   cast[ptr int](seqVar)
 
@@ -100,3 +111,176 @@ proc unsafeAdd*(seqVar: pointer; val: ptr byte; stride: int) =
   let data = unsafeSeqDataPtr(seqVar)
   copyMem(cast[pointer](cast[int](data) + length * stride), val, stride)
   seqLenPtr(seqVar)[] = length + 1
+
+# -------------------------------------------------------------------------------
+# Safe API
+# -------------------------------------------------------------------------------
+
+proc ensureCap[T](s: var VSeq[T], len: int) =
+  let header = sizeof(int)
+  if s.payload == nil: 
+    s.payload = cast[ptr VSeqPayload[T]](alloc0(header + len*sizeof(T)))
+    s.payload.cap = len
+  if s.payload.cap < len:
+    let newCap = len*2
+    let newData = realloc0(s.payload, header + s.payload.cap * sizeof(T), header + newCap * sizeof(T))
+
+    assert newData != nil, "Insufficient memory to grow VSeq."
+    s.payload = cast[ptr VSeqPayload[T]](newData)
+    s.payload.cap = newCap
+
+proc newVSeqOfCap*[T](cap: int): VSeq[T] =
+  result = VSeq[T](len: 0)
+  ensureCap(result, cap)
+
+proc newVSeq*[T](len: int): VSeq[T] =
+  result = VSeq[T](len: len)
+  ensureCap(result, len)
+
+proc len*[T](s: VSeq[T]) = s.len
+
+proc shrink*[T](s: var VSeq[T], newLen: int) =
+  assert newLen <= s.len, "Can't shrink to greater than the sequences length"
+  for i in newLen..<s.len:
+    s[i].reset()
+
+  s.len = newLen
+
+proc grow*[T](s: var VSeq[T], newLen: int) = 
+  assert newLen >= s.len, "Can't grow to lesser than the sequence length"
+  ensureCap(s, newLen)
+  s.len = newLen
+
+proc setLen*[T](s: var VSeq[T], newLen: int) =
+  if s.len < newLen:
+    grow(s, newLen)
+  else:
+    shrink(s, newLen)
+
+template `[]`*[T](s: VSeq[T], i: untyped): T =
+  when CHECKS_ENABLED: assert i >= 0 and i < s.len, "Access out of bound"
+  s.payload.data[i]
+
+template `[]`*[T](s: VSeq[T], i: BackwardsIndex): T =
+  s[s.len - i.int]
+
+template `[]=`*[T](s: var VSeq[T], i: untyped, v: T) =
+  when CHECKS_ENABLED: assert i >= 0 and i < s.len, "Access out of bound"
+  s.payload.data[i] = v
+
+template `[]=`*[T](s: var VSeq[T], i: BackwardsIndex, v: T) =
+  s[s.len - i.int] = v
+
+proc add*[T](s: var VSeq[T], v: T) =
+  s.grow(s.len+1)
+  s[s.len-1] = v
+
+proc add*[T](s: var VSeq[T], v: openArray[T]) =
+  let oldLen = s.len
+  s.grow(oldLen + v.len)
+
+  for i in oldLen..<s.len:
+    s[i] = v[i - oldLen]
+
+proc `&`*[T](s1, s2: VSeq[T]): VSeq[T] =
+  result = newVSeq[T](s1.len + s2.len)
+  for i in 0..<s1.len:
+    result[i] = s1[i]
+
+  for i in 0..<s2.len:
+    result[s1.len + i] = s2[i]
+
+proc pop*[T](s: var VSeq[T]): T =
+  when CHECKS_ENABLED: assert s.len > 0, "Can't pop on empty VSeq."
+  result = s[^1]
+  s[^1] = default(T)
+  dec s.len
+
+proc delete*[T](s: var VSeq[T], i: int) =
+  when CHECKS_ENABLED: assert i >= 0 and i < s.len, "Access out of bound"
+  for i in i..<s.len-1:
+    s[i] = s[i+1]
+
+  s[^1] = default(T)
+  dec s.len
+
+proc insert*[T](s: var VSeq[T], i: int, v: T) =
+  when CHECKS_ENABLED: assert i >= 0 and i < s.len, "Access out of bound"
+  s.grow(s.len+1)
+  for i in countdown(s.len-1, i+1):
+    s[i] = s[i-1]
+  s[i] = v
+
+
+proc del*[T](s: var VSeq[T], i: int) =
+  when CHECKS_ENABLED: assert i >= 0 and i < s.len, "Access out of bound"
+  s[i] = s[^1]
+  s[^1] = default(T)
+  s.shrink(s.len-1)
+
+proc toSeq*[T](s: VSeq[T]): seq[T] =
+  result = newSeq[T](s.len)
+  for i in 0..<s.len:
+    result[i] = s[i]
+
+proc toVSeq*[T](s: seq[T]): VSeq[T] =
+  result = newVSeq[T](s.len)
+  for i in 0..<s.len:
+    result[i] = s[i]
+
+proc clear*[T](s: var VSeq[T]) = 
+  s.setLen(0)
+
+iterator items*[T](s: VSeq[T]): T =
+  for i in 0..<s.len:
+    yield s[i]
+
+iterator mitems*[T](s: var VSeq[T]): var T =
+  for i in 0..<s.len:
+    yield s[i]
+
+iterator pairs*[T](s: VSeq[T]): (int, T) =
+  for i in 0..<s.len:
+    yield (i, s[i])
+
+iterator mpairs*[T](s: var VSeq[T]): (int, T) =
+  for i in 0..<s.len:
+    yield (i, s[i])
+  
+template destroy[T](s: var VSeq[T]) =
+  if s.payload != nil:
+    for i in 0..<s.len:
+      s[i].reset()
+
+    s.payload.dealloc()
+
+  s.len = 0
+
+proc `=destroy`*[T](s: var VSeq[T]) =
+  destroy(s)
+
+proc `=sink`*[T](dest: var VSeq[T]; src: VSeq[T]) =
+  if dest.payload != src.payload:
+    destroy(dest)
+  
+  dest.payload = src.payload
+  dest.len = src.len
+
+proc `=copy`*[T](dest: var VSeq[T]; src: VSeq[T]) =
+  if src.payload == dest.payload:
+    return
+  if src.payload == nil:
+    destroy(dest)
+  else:
+    let newCap = src.payload.cap
+
+    dest.setLen(src.len)
+    for i in 0..<src.len:
+      dest[i] = src[i]
+    
+proc `=dup`*[T](src: VSeq[T]): VSeq[T] =
+  if src.payload != nil:
+    result = newVSeqOfCap[T](src.payload.cap)
+    result.setLen(src.len)
+    for i in 0..<src.len:
+      result[i] = src[i]
